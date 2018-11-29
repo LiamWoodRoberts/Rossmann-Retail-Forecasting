615f2844bd8d64775796a4f12fdd15f2249ebd86
Liam Wood Roberts <Liamwr17@gmail.com>
2018-11-29 15:59:58 -0500

    Add files via upload

---
+++ model_metrics.py
@@ -0,0 +1,24 @@
+import h2o
+
+#Initialize h2o cluster
+h2o.init(nthreads=-1,max_mem_size='6G')
+h2o.remove_all()
+
+#Load Model/s
+forest_path ='/Users/liamroberts/Desktop/Datasets/Rossmann/Random_forest_model/DRF_model_python_1543508229496_1'
+rdf_model = h2o.load_model(forest_path)
+
+xg_path = '/Users/liamroberts/Desktop/Datasets/Rossmann/models/XGBoost_model_python_1543515884361_1'
+xg_model = h2o.load_model(xg_path)
+
+#Check Performance
+print('Random Forest Model')
+print(rdf_model.model_performance(train=True))
+print(rdf_model.model_performance(valid =True))
+
+print('XGBoost Model')
+print(xg_model.model_performance(train=True))
+print(xg_model.model_performance(valid =True))
+
+#Shutdown Cluster
+h2o.cluster().shutdown()
