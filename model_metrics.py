aa6f767187270eae062c36d6b85e1d532cdf32d0
Liam Wood Roberts <Liamwr17@gmail.com>
2018-11-29 23:10:36 -0500

    Minor formatting changes

--- model_metrics.py
+++ model_metrics.py
@@ -2,25 +2,30 @@
 
 import h2o
 
-#Initialize h2o cluster
-h2o.init(nthreads=-1,max_mem_size='6G')
+# Initialize h2o cluster
+h2o.init(nthreads=-1,
+         max_mem_size='6G')
 h2o.remove_all()
 
-#Load Model/s
-forest_path ='/Users/liamroberts/Desktop/Datasets/Rossmann/Random_forest_model/DRF_model_python_1543508229496_1'
-rdf_model = h2o.load_model(forest_path)
+# Load Models
+path = '/Users/LiamRoberts/rossmann_retail/models/'
 
-xg_path = '/Users/liamroberts/Desktop/Datasets/Rossmann/models/XGBoost_model_python_1543515884361_1'
-xg_model = h2o.load_model(xg_path)
+# Random Forest
+forest_path = 'DRF_model_python_1543549695844_1'
+rdf_model = h2o.load_model(f'{path}{forest_path}')
 
-#Check Performance
+# XG Boost
+xg_path = 'XGBoost_model_python_1543549100463_1'
+xg_model = h2o.load_model(f'{path}{xg_path}')
+
+# Check Performance
 print('Random Forest Model')
 print(rdf_model.model_performance(train=True))
-print(rdf_model.model_performance(valid =True))
+print(rdf_model.model_performance(valid=True))
 
 print('XGBoost Model')
 print(xg_model.model_performance(train=True))
-print(xg_model.model_performance(valid =True))
+print(xg_model.model_performance(valid=True))
 
-#Shutdown Cluster
+# Shutdown Cluster
 h2o.cluster().shutdown()
