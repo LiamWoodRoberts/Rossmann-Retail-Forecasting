70edd871ae3268ee3648cbfa3f8314b42e55c0ff
Liam Wood Roberts <Liamwr17@gmail.com>
2018-11-29 23:03:29 -0500

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,11 +2,14 @@
 
 import pandas as pd
 
-#Specify Working Directory
-directory = '/Users/liamroberts/Desktop/Datasets/Rossmann/'
+# Specify Working Directory
+directory = '/Users/LiamRoberts/Desktop/Professional/DataScience/Rossmann Retail Forecasting/'
 
-#Import Data
+# Import Data
 store = pd.read_csv(f'{directory}store.csv',
+                    low_memory=False)
+
+state = pd.read_csv(f'{directory}store_states.csv',
                     low_memory=False)
 
 df = pd.read_csv(f'{directory}train.csv',
@@ -14,49 +17,78 @@ df = pd.read_csv(f'{directory}train.csv',
                  parse_dates=True,
                  index_col='Date')
 
-#Encode Data Times
+# Encode Data Times
 df['Month'] = df.index.month
 df['WeekOfYear'] = df.index.weekofyear
 
-#Merge Data Frames
-df = pd.merge(df,store,on = 'Store')
+# Merge Data Frames
+df = pd.merge(df,
+              store,
+              on = 'Store')
 
-#Remove Days Where Sales are Zero
-df = df[(df['Sales']>0)| (df['Open']==0)]
+df = pd.merge(df,
+              state,
+              on = 'Store')
 
-#-----Feature Engineering------#
+# Remove days where sales are zero and when store is Closed
+df = df[(df['Sales'] > 0)]
+df = df[(df['Open'] == 1)]
 
-#Store Specific Metrics
+# -----Feature Engineering------ #
+
+# Store Specific Metrics
 agg_df = df.groupby(by='Store').mean()
 agg_df['SalesPerCustomer'] = agg_df['Sales']/agg_df['Customers']
 agg_df['MeanStoreSales'] = agg_df['Sales']
 agg_df['MeanStoreCustomers'] = agg_df['Customers']
-agg_df = agg_df[['MeanStoreSales','MeanStoreCustomers','SalesPerCustomer']]
-df = pd.merge(df,agg_df, on = 'Store')
+agg_df = agg_df[['MeanStoreSales',
+                 'MeanStoreCustomers',
+                 'SalesPerCustomer']]
+df = pd.merge(df,
+              agg_df,
+              on='Store')
 
-#Promo Sales Ratio
-promo = df[df['Promo']==1].groupby('Store').mean()
-nopromo = df[df['Promo']==0].groupby('Store').mean()
+# Promo Sales Ratio
+promo = df[df['Promo'] == 1].groupby('Store').mean()
+nopromo = df[df['Promo'] == 0].groupby('Store').mean()
 promo['PromoRatio'] = promo['Sales']/nopromo['Sales']
 promo['A'] = promo['Sales']
-promo = promo[['PromoRatio','A']]
-df = pd.merge(df,promo,on = 'Store')
+promo = promo[['PromoRatio',
 
-#Promo Days Ratio
-promo = df[df['Promo']==1].groupby('Store').sum()
-nopromo = df[df['Promo']==0].groupby('Store').sum()
+               'A']]
+
+df = pd.merge(df,
+              promo,
+              on='Store')
+
+# Promo Days Ratio
+promo = df[df['Promo'] == 1].groupby('Store').sum()
+nopromo = df[df['Promo'] == 0].groupby('Store').sum()
 promo['PromoDaysRatio'] = promo['Open']/nopromo['Open']
 promo['B'] = promo['Sales']
-promo = promo[['PromoDaysRatio','B']]
-df = pd.merge(df,promo,on = 'Store')
-df.drop(columns = ['A','B'],inplace=True)
+promo = promo[['PromoDaysRatio',
+               'B']]
 
-#------Save Data to Csv------#
-#Train test split ~90% through data
+df = pd.merge(df,
+              promo,
+              on='Store')
+
+df.drop(columns=['A',
+                 'B',
+                 'Promo2SinceWeek',
+                 'Promo2SinceYear'],
+        inplace=True)
+
+# ------Save Data to Csv------ #
+
+# Train test split ~90% through data
 split_point = int(len(df)*0.9)
 train = df[:split_point]
 test = df[split_point:]
 
-#Save train and test to local folder
-train.to_csv('train.csv',index=False)
-test.to_csv('test.csv',index=False)
+# Save train and test to local folder
+train.to_csv('train.csv',
+             index=False)
+test.to_csv('test.csv',
+            index=False)
+print("~Files Succesfully Saved~")
